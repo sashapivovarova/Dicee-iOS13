20111d47272d0a45e297673326c142e78eae1fb4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-03-02 22:14:22 +0900

    diceImageViewOne の追加

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
